bff1cd26d8262ad40dc4bedc23a7e7b419c957d3
VijayVJYO <105290970+VijayVJYO@users.noreply.github.com>
2022-05-10 18:46:40 +0530

    Add files via upload

---
+++ AmstrongNumber.java
@@ -0,0 +1,22 @@
+package selenium;
+
+public class AmstrongNumber {
+
+		public static void main(String[] args) {
+			int num=370;
+			int temp=num,r,sum=0;
+			while (num>0) {
+				r=num%10;
+				num/=10;
+				sum=sum+r*r*r;	
+	}
+			if (temp==sum) {
+				System.out.println("its a armstrong");
+	} 
+			else {
+				System.out.println("its not a amstrong");
+				}
+	}
+	}
+
+
---
+++ SumOfDigits.java
@@ -0,0 +1,19 @@
+package selenium;
+
+public class SumOfDigits {
+	
+		public static void main(String[] args) {
+			int input=654;
+			int num=0;
+			int sum;
+			while (input>0) {
+				sum=input%10;
+				num=num+sum;
+				input=input/10;
+			}
+			System.out.println(num);
+			
+			}
+	}
+
+
